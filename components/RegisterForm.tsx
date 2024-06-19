c146a5d9012bb111f60e5d25a7782440d322f9bf
ahrimaz <eric.stanard@gmail.com>
2024-06-19 17:03:32 -0400

    feat: Add RegisterPage and RegisterForm components

---
+++ components/RegisterForm.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useState, ChangeEvent } from 'react';
+
+const RegisterForm: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleRegister = async () => {
+    try {
+      const response = await fetch('https://energetic-tidy-ray.glitch.me/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Registration successful:', data);
+        // successful registration
+      } else {
+        const errorData = await response.json();
+        console.error('Registration failed:', errorData);
+        // failed registration
+      }
+    } catch (error: any) {
+      console.error('Error registering user:', error.message);
+      // Handle network or other errors
+    }
+  };
+
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+};
+
+export default RegisterForm;
