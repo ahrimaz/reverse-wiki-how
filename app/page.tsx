0ad40fb8108eefe9a9c7d7385d5ddb2b4c16031c
ahrimaz <eric.stanard@gmail.com>
2024-06-20 15:56:36 -0400

    chore: Refactor ChatClient component to improve socket connection handling and use ChakraProvider for styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import { Box, Flex } from "@chakra-ui/react";
 import SlideShow from "@/components/SlideShow";
 import ChatClient from "@/components/ChatClient";
-import { useAuth } from "@/hooks/useAuth";
 
 const images = [
   "https://www.wikihow.com/images/thumb/8/83/Prevent-or-Survive-a-Monkey-Attack-Step-1-Version-4.jpg/aid56064-v4-728px-Prevent-or-Survive-a-Monkey-Attack-Step-1-Version-4.jpg.webp",
@@ -13,7 +10,6 @@ const images = [
 ]
 
 export default function Home() {
-  const { user } = useAuth();
 
   return (
     <>
