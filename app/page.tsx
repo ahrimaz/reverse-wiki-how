afe8dc40fab4756680a3a037ec66b41b130efb99
ahrimaz <eric.stanard@gmail.com>
2024-06-20 15:14:39 -0400

    feat: Update ChatClient to display authenticated username in ChatClient component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { Box, Flex } from "@chakra-ui/react";
 import SlideShow from "@/components/SlideShow";
 import ChatClient from "@/components/ChatClient";
+import { useAuth } from "@/hooks/useAuth";
 
 const images = [
   "https://www.wikihow.com/images/thumb/8/83/Prevent-or-Survive-a-Monkey-Attack-Step-1-Version-4.jpg/aid56064-v4-728px-Prevent-or-Survive-a-Monkey-Attack-Step-1-Version-4.jpg.webp",
@@ -10,11 +13,13 @@ const images = [
 ]
 
 export default function Home() {
+  const { user } = useAuth();
+
   return (
     <>
     <Flex align="center" justify="center" p={4} m={4}>
     <SlideShow images={images} />
-    <ChatClient username={""} />
+    <ChatClient username={user?.username ?? ''} />
     </Flex>
     </>
   );
