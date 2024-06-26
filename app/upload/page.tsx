ce663b9ba3b27971d93e32af870631e86283de18
ahrimaz <eric.stanard@gmail.com>
2024-06-26 16:19:04 -0400

    feat: Update AboutPage heading to "Upload images here"

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -6,14 +6,8 @@ const AboutPage: React.FC = () => {
     return (
         <Box p={4}>
             <Heading as="h1" size="xl" mb={4}>
-                About
+                Upload images here
             </Heading>
-            <Text fontSize="lg">
-                Welcome!
-            </Text>
-            <Text fontSize="lg" mt={4}>
-            This is a simple application inspired by Griffin McElroy&apos;s reverse wiki how game.
-            </Text>
             <FileUpload />
         </Box>
     );
